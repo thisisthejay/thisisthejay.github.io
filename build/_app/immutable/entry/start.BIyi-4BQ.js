00adb1303a0e4c470634a986dcb61c1cdac0ec4a
thisisthejay <jermainepaulsmith@icloud.com>
2024-12-22 12:07:23 +0000

    optimised carousel images

--- build/_app/immutable/entry/start.BIyi-4BQ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.05TqLDoQ.js";export{t as start};
--- src/lib/components/carousel.svelte
+++ src/lib/components/carousel.svelte
@@ -1,18 +1,18 @@
 <div class="carousel rounded-box" id="item1">
 	<div class="carousel-item">
-		<img class="max-h-96" src="images/where_does_your_story_really_begin.jpg" alt="Burger" />
+		<img class="max-h-96" src="images/where_does_your_story_really_begin-min.jpg" alt="Burger" />
 	</div>
 	<div class="carousel-item" id="item2">
-		<img class="max-h-96" src="images/hugs.jpg" alt="Burger" />
+		<img class="max-h-96" src="images/hugs-min.jpg" alt="Burger" />
 	</div>
 	<div class="carousel-item" id="item3">
-		<img class="max-h-96" src="images/a_purposeful_journey.jpg" alt="Burger" />
+		<img class="max-h-96" src="images/a_purposeful_journey-min.jpg" alt="Burger" />
 	</div>
 	<div class="carousel-item" id="item4">
-		<img class="max-h-96" src="images/i_am_speaking.jpg" alt="Burger" />
+		<img class="max-h-96" src="images/i_am_speaking-min.jpg" alt="Burger" />
 	</div>
 </div>
-<!-- <div class="flex w-full justify-center gap-2 py-2">
+<!-- <div class="flex justify-center w-full gap-2 py-2">
 	<a href="#item1" class="btn btn-xs badge-secondary">1</a>
 	<a href="#item2" class="btn btn-xs badge-secondary">2</a>
 	<a href="#item3" class="btn btn-xs badge-secondary">3</a>
