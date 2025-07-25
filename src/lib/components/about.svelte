5d7d1158e3009e5e59678ce11f7d426d27ad1e0d
thisisthejay <jermainepaulsmith@icloud.com>
2025-07-25 09:55:38 +0100

    update roles

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -4,59 +4,102 @@
 
 <div class="bg-base-100 pb-24" id="about">
 	<div class="mx-auto max-w-2xl lg:max-w-4xl">
-		<h2 class="text-base/7 font-semibold text-secondary">Allow me to introduce myself</h2>
-		<p class="text-4xl max-w-2xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100l">
+		<h2 class="text-base/7 font-semibold text-secondary">
+			Allow me to introduce myself
+		</h2>
+		<p
+			class="text-4xl max-w-2xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100l"
+		>
 			this is the work I am here to do
 		</p>
-		<div class="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+		<div
+			class="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2"
+		>
 			<!-- Scroll in from left -->
 			<div class="flex p-px lg:col-span-4">
 				<div
 					class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"
 				>
 					<div class="p-10">
-						<h3 class="text-sm/4 font-semibold text-secondary">Legal Operations and Engineering</h3>
-						<p class="mt-2 text-lg font-medium tracking-tight text-white">Transforming Law</p>
+						<h3 class="text-sm/4 font-semibold text-secondary">
+							Legal Operations and Engineering
+						</h3>
+						<p
+							class="mt-2 text-lg font-medium tracking-tight text-white"
+						>
+							Transforming Law
+						</p>
 						<p class="mt-2 max-w-lg text-sm/6 text-gray-400">
-							Leader in process improvement, integrating LegalTech tools to streamline legal operations and boost firm
-							efficiency, with experience overseeing projects at top law firms.
+							Leader in process improvement, integrating LegalTech
+							tools to streamline legal operations and boost firm
+							efficiency, with experience overseeing projects at
+							top law firms and global organisations.
 						</p>
 						<dl class="grid grid-cols-1 gap-1 mt-2">
-							<dt class="text-accent text-xs">Head of Legal Operations</dt>
-							<dt class="text-accent text-xs">Senior Legal Operations Manager</dt>
+							<dt class="text-accent text-xs">
+								Head of Legal Operations
+							</dt>
+							<dt class="text-accent text-xs">
+								Senior Legal Operations Manager
+							</dt>
 							<dt class="text-accent text-xs">Legal Engineer</dt>
+							<dt class="text-accent text-xs">
+								Legal Operations Managing Counsel
+							</dt>
 						</dl>
 					</div>
 				</div>
 			</div>
 			<!-- Scroll in from Right -->
 			<div class="flex p-px lg:col-span-2">
-				<div class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-tr-[2rem]">
+				<div
+					class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-tr-[2rem]"
+				>
 					<div class="p-10">
-						<h3 class="text-sm/4 font-semibold text-secondary">Software</h3>
-						<p class="mt-2 text-lg font-medium tracking-tight text-white">Intergrating Technology</p>
+						<h3 class="text-sm/4 font-semibold text-secondary">
+							Software
+						</h3>
+						<p
+							class="mt-2 text-lg font-medium tracking-tight text-white"
+						>
+							Intergrating Technology
+						</p>
 						<p class="mt-2 max-w-lg text-sm/6 text-gray-400">
-							Skilled in multiple programming languages and identifying / procuring market-leading tools to create
-							scalable legal tech solutions.
+							Skilled in multiple programming languages and
+							identifying / procuring market-leading tools to
+							create scalable legal tech solutions.
 						</p>
 						<dl class="grid grid-cols-1 gap-1 mt-2">
-							<dt class="text-accent text-xs">BCS Certificate in Requirements Engineering</dt>
+							<dt class="text-accent text-xs">
+								BCS Certificate in Requirements Engineering
+							</dt>
 						</dl>
 					</div>
 				</div>
 			</div>
 			<!-- Scroll in from Left -->
 			<div class="flex p-px lg:col-span-2">
-				<div class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-bl-[2rem]">
+				<div
+					class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-bl-[2rem]"
+				>
 					<div class="p-10">
-						<h3 class="text-sm/4 font-semibold text-secondary">Entrepreneurship</h3>
-						<p class="mt-2 text-lg font-medium tracking-tight text-white">Creating Change</p>
+						<h3 class="text-sm/4 font-semibold text-secondary">
+							Entrepreneurship
+						</h3>
+						<p
+							class="mt-2 text-lg font-medium tracking-tight text-white"
+						>
+							Creating Change
+						</p>
 						<p class="mt-2 max-w-lg text-sm/6 text-gray-400">
-							Founder of SenseCheck Technologies, taking a disjointed process and creating an AML platform and
+							Founder of SenseCheck Technologies, taking a
+							disjointed process and creating an AML platform and
 							communication tool for Estate Planning lawyers.
 						</p>
 						<dl class="grid grid-cols-1 gap-1 mt-2">
-							<dt class="text-accent text-xs">Series A in 2024</dt>
+							<dt class="text-accent text-xs">
+								Series A in 2024
+							</dt>
 						</dl>
 					</div>
 				</div>
@@ -67,14 +110,24 @@
 					class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]"
 				>
 					<div class="p-10">
-						<h3 class="text-sm/4 font-semibold text-secondary">Intelligence</h3>
-						<p class="mt-2 text-lg font-medium tracking-tight text-white">Building for the future</p>
+						<h3 class="text-sm/4 font-semibold text-secondary">
+							Intelligence
+						</h3>
+						<p
+							class="mt-2 text-lg font-medium tracking-tight text-white"
+						>
+							Building for the future
+						</p>
 						<p class="mt-2 max-w-lg text-sm/6 text-gray-400">
-							Focused on using AI to improve drafting, advice and legal operations. Leveraging AI with the right
+							Focused on using AI to improve drafting, advice and
+							legal operations. Leveraging AI with the right
 							opportunities.
 						</p>
 						<dl class="grid grid-cols-1 gap-1 mt-2">
-							<dt class="text-accent text-xs">Standford Graduate Business School Certificate in AI Essentials</dt>
+							<dt class="text-accent text-xs">
+								Standford Graduate Business School Certificate
+								in AI Essentials
+							</dt>
 						</dl>
 					</div>
 				</div>
